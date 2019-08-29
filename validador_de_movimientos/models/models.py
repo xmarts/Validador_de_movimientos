138aeff77e30d69d9ee2c3ee3b1850375a76a69d
luis <angelchicles97@gmail.com>
2019-08-29 16:15:58 -0500

    validaciones dobles

--- validador_de_movimientos/models/models.py
+++ validador_de_movimientos/models/models.py
@@ -4,9 +4,23 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 class bloqueo_mercancai(models.Model):
+    _inherit = 'stock.move'
+
+    def button_validates(self):
+        for record in self:
+            if record.move_line_ids:
+                con = 0
+                for line in record.move_line_ids:
+                    if line.qty_done > line.product_uom_qty:
+                        con =+ 1
+                if con > 0:        
+                    raise ValidationError('El producto no corresponde a la demanda inicial, favor de verificar.')
+                else:     
+                    return
+class bloqueo_principal(models.Model):
     _inherit = 'stock.picking'
 
-    def button_validate(self):
+    def button_vali(self):
         for record in self:
             if record.move_ids_without_package:
                 con = 0
@@ -16,4 +30,4 @@ class bloqueo_mercancai(models.Model):
                 if con > 0:        
                     raise ValidationError('El producto no corresponde a la demanda inicial, favor de verificar.')
                 else:     
-                    return super(bloqueo_mercancai, self).button_validate()
+                    self.button_validate()
